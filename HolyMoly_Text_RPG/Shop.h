07d278233dc0bb7b681d10b82fce61b86d9a3c83
lambone23 <lambone234567@gmail.com>
2025-06-20 17:53:13 +0900

    update shop, common
    - shop
    OnEnter 함수 추가,
    DisplayItems 함수 update
    BuyItem 함수 update
    
    - common.h
    FnIsNumber 함수 추가

--- HolyMoly_Text_RPG/Shop.h
+++ HolyMoly_Text_RPG/Shop.h
@@ -1,18 +1,29 @@
 #pragma once
 #include "common.h"
+#include "Item.h"
 
-class Item;
 class Character;
 
 class Shop
 {
 public:
 	Shop();
-	void DisplayItems();
-	void BuyItem(int index, Character* player);
-	void SellItem(int index, Character* player);
+
+	// 상점 진입
+	void OnEnter(Character* player);
+
+	// 표시 - 판매중인 아이템 목록
+	void DisplayItems(Character* player);
+
+	// 구매
+	void BuyItem(Character* player);
+
+	// 판매
+	void SellItem(Character* player);
 
 private:
-	//vector<Item*> availableItems;
+
+	// 판매중인 아이템
 	vector<unique_ptr<Item>> availableItems;
+	//vector<Item*> availableItems;
 };
