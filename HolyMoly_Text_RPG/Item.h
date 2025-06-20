597e1d11e622b27d3ded45964fb8acc94e0bc3cf
oophs <116652521+DUDhwana@users.noreply.github.com>
2025-06-20 11:06:23 +0900

    Update Item.h
    중복된 #pragma once 제거

--- HolyMoly_Text_RPG/Item.h
+++ HolyMoly_Text_RPG/Item.h
@@ -10,8 +10,8 @@ class Item
 public:
 	virtual ~Item() {}
 
-	virtual string getName() = 0;             // 아이템 이름
-	virtual int getPrice() = 0;               // 아이템 가격
-	virtual int getIncreseAmount() = 0;       // 효과 수치 (회복량, 공격력 등)
+	virtual string getName() = 0;             	// 아이템 이름
+	virtual int getPrice() = 0;               	// 아이템 가격
+	virtual int getIncreseAmount() = 0;       	// 효과 수치 (회복량, 공격력 등)
 	virtual void use(Character* character) = 0; // 캐릭터에게 아이템 사용
 };
