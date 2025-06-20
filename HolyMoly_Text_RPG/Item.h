2705fa2f8692cfe1c1fd38a855b7744540167a46
oophs <116652521+DUDhwana@users.noreply.github.com>
2025-06-20 19:14:30 +0900

    Update Item.h
    오타 수정

--- HolyMoly_Text_RPG/Item.h
+++ HolyMoly_Text_RPG/Item.h
@@ -6,19 +6,11 @@ class Character;
 
 class Item
 {
-private:
-	string name;
-	int value;
-
 public:
 	virtual ~Item() {}
-
 	virtual string getName() = 0;             	// 아이템 이름
 	virtual int getPrice() = 0;               	// 아이템 가격
 	virtual int getIncreaseAmount() = 0;       	// 효과 수치 (회복량, 공격력 등)
 	virtual void use(Character* character) = 0; // 캐릭터에게 아이템 사용
 	virtual void display() const = 0;			// 아이템 정보 콘솔에 출력
 };
-
-class HealthPotion;
-Class AttackBoost;
