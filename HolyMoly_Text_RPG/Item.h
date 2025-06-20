3e805b85a63e45f6b4443d05deaaaaf2564e056e
oophs <116652521+DUDhwana@users.noreply.github.com>
2025-06-20 16:27:07 +0900

    Update Item.h
    아이템 추상 클래스 정의 및 멤버 변수 추가

--- HolyMoly_Text_RPG/Item.h
+++ HolyMoly_Text_RPG/Item.h
@@ -1,5 +1,4 @@
 #pragma once
-#pragma once
 #include <string>
 using namespace std;
 
@@ -12,6 +11,10 @@ public:
 
 	virtual string getName() = 0;             	// 아이템 이름
 	virtual int getPrice() = 0;               	// 아이템 가격
-	virtual int getIncreseAmount() = 0;       	// 효과 수치 (회복량, 공격력 등)
+	virtual int getIncreaseAmount() = 0;       	// 효과 수치 (회복량, 공격력 등)
 	virtual void use(Character* character) = 0; // 캐릭터에게 아이템 사용
+	virtual void display() const = 0;			// 아이템 정보 콘솔에 출력
 };
+
+class HealthPotion;
+Class AttackBoost;
