12e147671ad932b0fa31b004ff7b34152994d36a
chaenning <codmsrjf@naver.com>
2025-06-24 10:59:08 +0900

    Update Orc.cpp
     - 몬스터 이름 Orc -> Orc Mage 변경
     - 아스키 아트 변경

--- HolyMoly_Text_RPG/Orc.cpp
+++ HolyMoly_Text_RPG/Orc.cpp
@@ -1,7 +1,7 @@
-#include "Orc.h"
+﻿#include "Orc.h"
 
 Orc::Orc(int level) : Monster(level) {
-	name = "Orc";
+	name = "Orc Mage";
 	gold = RandomUtil::GetRandomInt(7, 15);
 }
 
@@ -14,4 +14,33 @@ Item* Orc::dropItem()
 int Orc::dropGold()
 {
 	return gold;
+}
+
+void Orc::displayMonster() {
+    vector<string> MonsterAsciiArt = {
+    "  ,    ,    /\\   /\\",
+    "  /( /\\ )\\  _\\ \\_/ /_",
+    "  |\\_||_/| < \\_   _/ >",
+    "  \\______/  \\|0   0|/",
+    "    _\\/_   _(_  ^  _)_",
+    "     ( () ) /`\\|V\"\"\"V|/`\\",
+    "       {}   \\  \\_____/  /",
+    "       ()   /\\   )=(   /\\",
+    "        {}  /  \\_/\\=/\\_/  \\"
+    };
+
+
+    int consoleWidth = 90;
+
+    FnSetTextColor(EColors::RED);
+    for (auto& s : MonsterAsciiArt)
+    {
+        int s_len = s.length();
+        int padding = (consoleWidth - s_len) / 2;
+        cout << setw(padding + s_len) << right << s << endl;
+        FnSleep(100);
+    }
+
+    cout << endl;
+    FnSetTextDefault();
 }
