3ed72b74bb8eb28f0271e9adcecbb75134571eb7
yubi11 <yubi11@naver.com>
2025-06-25 12:28:45 +0900

    update character.cpp, holywater.cpp
    성수 및 캐릭터 밸런스패치. 레벨업 시 최대체력 증가량만큼은 체력 회복

--- HolyMoly_Text_RPG/HolyWater.cpp
+++ HolyMoly_Text_RPG/HolyWater.cpp
@@ -12,11 +12,11 @@ void HolyWater::use(Character* character) {
     int currentHealth = character->getHealth();
     int currentAttack = character->getAttack();
     character->setHealth(currentHealth + value);
-    character->setAttack(currentAttack + (value/10));
-    cout << character->getName() << "이(가) " << name << "을(를) 사용하여 축복을 받았습니다.(체력: " << value <<", 공격력: " << value/10 << "상승" << endl;
+    character->setAttack(currentAttack + (value/5));
+    cout << character->getName() << "이(가) " << name << "을(를) 사용하여 축복을 받았습니다.(체력: " << value <<", 공격력: " << value/5 << "상승" << endl;
 }
 
 void HolyWater::display() const {
-    cout << "[아이템 : " << name << "] 효과 : 체력, 공격력 +" << value
+    cout << "[아이템 : " << name << "] 효과 : 체력 + "<< value << ", 공격력 + " << value/5
         << " / 가격 : " << price << " Gold" << endl;
 }
