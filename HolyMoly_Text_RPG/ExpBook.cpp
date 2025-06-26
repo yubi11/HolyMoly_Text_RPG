cc6001de1a34d439f1d8689c780addd9df8a3fff
yubi11 <yubi11@naver.com>
2025-06-26 12:17:05 +0900

    Update ExpBook.cpp
    경험치 획득 로그 2번 출력 수정

--- HolyMoly_Text_RPG/ExpBook.cpp
+++ HolyMoly_Text_RPG/ExpBook.cpp
@@ -9,8 +9,9 @@ string ExpBook::getName() const { return name; }
 int ExpBook::getPrice() const { return price; }
 int ExpBook::getItemValue() const { return value; }
 void ExpBook::use(Character* character) { // Character 클래스 내의 addExperience 함수를 통해 경험치 책 사용으로 얻는 경험치 증가 및 레벨업 동시처리
+    cout << character->getName() << "이(가) " << name << "을(를) 사용하여 ";
     character->addExperience(value);
-    cout << character->getName() << "이(가) " << name << "을(를) 사용하여 " << value << "경험치를 획득했습니다." << endl;
+    cout << endl;
 }
 void ExpBook::display() const {
     cout << "[아이템: " << name << "] 효과: 경험치 +" << value
