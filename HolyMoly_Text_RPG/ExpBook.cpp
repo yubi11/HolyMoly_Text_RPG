8edd764599b59fa2c7f5164e8c62930b5ef771c2
SKim041 <sehee1806@gmail.com>
2025-06-26 12:35:44 +0900

    fix
    로그 수정
    GameManager
    - 배틀 아이템 사용 후 \n 추가

--- HolyMoly_Text_RPG/ExpBook.cpp
+++ HolyMoly_Text_RPG/ExpBook.cpp
@@ -11,7 +11,6 @@ int ExpBook::getItemValue() const { return value; }
 void ExpBook::use(Character* character) { // Character 클래스 내의 addExperience 함수를 통해 경험치 책 사용으로 얻는 경험치 증가 및 레벨업 동시처리
     cout << character->getName() << "이(가) " << name << "을(를) 사용하여 ";
     character->addExperience(value);
-    cout << endl;
 }
 void ExpBook::display() const {
     cout << "[아이템: " << name << "] 효과: 경험치 +" << value
